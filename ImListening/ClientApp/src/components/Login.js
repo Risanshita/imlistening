b84543125c75b2967582e547ad4ad81bb456507d
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-07-24 12:28:07 +0530

    Login page Style change

--- ImListening/ClientApp/src/components/Login.js
+++ ImListening/ClientApp/src/components/Login.js
@@ -1,127 +1,130 @@
-import { Button, Checkbox, Form, Input, Row, message,Col } from 'antd';
+import { Button, Checkbox, Form, Input, Row, message, Col } from "antd";
 import "./LoginStyle.css";
 const Login = (props) => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const onFinish = (values) => {
-        console.log('Success:', values);
-        login(values);
-    };
+  const [messageApi, contextHolder] = message.useMessage();
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    login(values);
+  };
 
-    const login = async (values) => {
-        try {
-            try {
-                const response = await fetch('api/users/authenticate', {
-                    body: JSON.stringify(values),
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                const responseData = await response.json();
-                if (!response.ok) {
-                    const msg = responseData.message ? responseData.message : Object.values(responseData.errors)[0][0];
-                    if (msg) {
-                        messageApi.open({
-                            type: 'error',
-                            content: msg,
-                        });
-                    }
-                } else {
-                    responseData.authdata = window.btoa(values.username + ':' + values.password);
-                    localStorage.setItem('user', JSON.stringify(responseData));
-                    messageApi.success({
-                        type: 'success',
-                        content: 'Login successful!',
-                    });
-                    console.log(props);
-                    window.location.href = '/';
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
+  const login = async (values) => {
+    try {
+      try {
+        const response = await fetch("api/users/authenticate", {
+          body: JSON.stringify(values),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const responseData = await response.json();
+        if (!response.ok) {
+          const msg = responseData.message
+            ? responseData.message
+            : Object.values(responseData.errors)[0][0];
+          if (msg) {
+            messageApi.open({
+              type: "error",
+              content: msg,
+            });
+          }
+        } else {
+          responseData.authdata = window.btoa(
+            values.username + ":" + values.password
+          );
+          localStorage.setItem("user", JSON.stringify(responseData));
+          messageApi.success({
+            type: "success",
+            content: "Login successful!",
+          });
+          console.log(props);
+          window.location.href = "/";
         }
-    };
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    return (<>
-        {contextHolder}
-       <Row justify="center" align="middle" style={{ height: '100vh' }}>
-       <Col>
-         <h2  className="loginHeading">Login</h2>
-       <Form
+  return (
+    <>
+      {contextHolder}
+      <Row justify="center" align="middle" style={{ height: "90vh" }}>
+        <Col className="LoginBox">
+          <h2 className="loginHeading">ACCOUNT LOGIN</h2>
+          <Form
             name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 16,
-            }}
-            style={{
-                maxWidth: 600,
-            }}
-            initialValues={{
-                remember: true,
-            }}
+            // labelCol={{
+            //   span: 8,
+            // }}
+            // wrapperCol={{
+            //   span: 16,
+            // }}
+            // style={{
+            //   maxWidth: 600,
+            // }}
+            // initialValues={{
+            //   remember: true,
+            // }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete="off"
-        >
+          >
+             <h4 className="lable">USERNAME</h4>
             <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
+            //   label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
             >
-                <Input />
+              <Input className="userNameField"/>
+            </Form.Item>
+
+            <h4 className="lable">PASSWORD</h4>
+            <Form.Item
+            //   label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password  className="pwdNameField"/>
             </Form.Item>
 
             <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
+              name="remember"
+              valuePropName="checked"
             >
-                <Input.Password />
+              <Checkbox className="Checkbox">Remember me</Checkbox>
             </Form.Item>
 
             <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
+            //   wrapperCol={{
+            //     offset: 8,
+            //     span: 16,
+            //   }}
             >
-                <Checkbox>Remember me</Checkbox>
+              <Button className="loginBtn" type="primary" htmlType="submit">
+                LOGIN
+              </Button>
             </Form.Item>
-
-            <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-       </Col>
-        </Row>
+          </Form>
+        </Col>
+      </Row>
     </>
-    );
-}
+  );
+};
 export default Login;
