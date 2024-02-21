563780f92a9acd53afe64441ab61d3d59100ffac
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-02-21 19:46:54 +0530

    Home Page introduction added

--- ImListening/ClientApp/src/components/Home.js
+++ ImListening/ClientApp/src/components/Home.js
@@ -1,10 +1,33 @@
-import def from "ajv/dist/vocabularies/applicator/additionalItems"
-
+import def from "ajv/dist/vocabularies/applicator/additionalItems";
+import { Button, Col, Row, Image } from "antd";
+import "./Home_style.css";
+import websitePreview from "../assets/images/websiteImage.png";
 const Home = () => {
-return <>
+  return (
+    <>
+      <Col>
+        <div className="headingBox">
+          <div className="homeTitle top"> Powerful tool </div>
+          <div className="homeTitle"> For Developers </div>
+          <div className="homeDes">Test your endpoint and </div>
+          <div className="tryButton">
+            <Button type="primary" className="homeBtn">
+              Try Now
+            </Button>
+          </div>
+        </div>
 
-</>
-
-}
+        <div className="imageBox">
+          <Image
+            preview={false}
+            src={websitePreview}
+            alt="website preview"
+            className="imagePreview"
+          />
+        </div>
+      </Col>
+    </>
+  );
+};
 
 export default Home;
